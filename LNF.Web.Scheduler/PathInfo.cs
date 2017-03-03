670c35a4b9514ddbd3065ced5231d5c24fff897b
Jim Getty <jgett@umich.edu>
2017-03-03 10:57:22 -0500

    Added extra delimeters for parsing PathInfo.

--- LNF.Web.Scheduler/PathInfo.cs
+++ LNF.Web.Scheduler/PathInfo.cs
@@ -1,8 +1,23 @@
-﻿using LNF.Cache;
+﻿/*
+  Copyright 2017 University of Michigan
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+using LNF.Cache;
 using LNF.Models.Scheduler;
 using LNF.Scheduler;
 using System.Configuration;
-using System.Linq;
 using System.Web;
 
 namespace LNF.Web.Scheduler
@@ -34,7 +49,7 @@ namespace LNF.Web.Scheduler
             if (string.IsNullOrEmpty(value))
                 return result;
 
-            string[] splitter = value.Split(char.Parse(PathInfo.PathDelimiter));
+            string[] splitter = value.Split(new char[] { char.Parse(PathDelimiter), ',', ':', '-', '|', '~' });
 
             if (splitter.Length == 0)
                 return result;
